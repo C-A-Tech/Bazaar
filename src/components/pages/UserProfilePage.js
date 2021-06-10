d1d322d7953ec34a3636a43b140dbb72ac637098
fizzy-fifs <fkinyemi@gmail.com>
2021-06-10 20:05:54 +0100

    Created pop up modal for requesting a stall

--- src/components/pages/UserProfilePage.js
+++ src/components/pages/UserProfilePage.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import axios from 'axios';
 import cookies from '../Cookies';
 import { Redirect } from 'react-router';
 
+Modal.setAppElement('#root')
 function UserProfilePage() {
   let id;
   let first_name;
@@ -24,11 +26,13 @@ function UserProfilePage() {
   }
 
 
-  function requestAStall(event){
-    console.log('inside the requestAStall function')
-    return <Redirect to="/user/:id/request-a-stall" />
+  function requestAStall() {
+    
 
   }
+
+
+  let [modalState, setmodalState] = useState(false)
 
   return (
     <div className="profile-container">
@@ -47,10 +51,43 @@ function UserProfilePage() {
 
 
       <div className="request-a-stall">
-        <form onSubmit={requestAStall()}>
-          <input type="submit" value= "Request A Stall" />
-        </form>
+        <button onClick={() => setmodalState(true)}> Request A Stall</button>
       </div>
+
+     {/* ......................................................... */}
+      <div className="stall-modal">
+
+        <Modal 
+          isOpen={modalState} 
+          onRequestClose={() => setmodalState(false)} 
+          style={
+            { overlay: {backgroundColor: 'grey'} }
+          }>
+          <input type="text" placeholder="Enter Stall Name"/> <br /><br />
+          
+          <select>
+            <option default>Select an appropriate section</option>
+            
+            <option value="antiques">Antiques</option>
+            <option value="carpets_and_rugs">Carpets and Rugs</option>
+            <option value="ceramics">Ceramics</option>
+            <option value="clothes">Clothes</option>
+            <option value="furnitures">Furnitures</option>
+            <option value="home_decor">Home Decor</option>
+            <option value="jewellery">Jewellery</option>
+
+          </select> <br /><br /><br />
+          
+          <h4> List Items For Sale</h4>
+          <input type="text" placeholder="Enter Item Name"/> <br /><br />
+          £ <input type="text" placeholder="Set Item Price"/> <br /><br />
+          <textarea placeholder="Enter Item Description" /> <br />
+
+          <input type="submit" value="Submit" />
+        </Modal>
+
+      </div>
+      
           
     
     </div>
