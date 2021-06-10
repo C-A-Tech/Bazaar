9f64fb8280b4146791ecbeeea5c4a6ba4d5ecdb1
Abdur Rehman <wabdulrehman@yahoo.com>
2021-06-10 16:33:32 +0100

    get api for stalls

--- src/components/pages/sections/Sections.js
+++ src/components/pages/sections/Sections.js
@@ -1,30 +1,27 @@
 import React, {useState} from 'react';
 import './sections.css';
+import axios from 'axios';
 
 function Sections() {
   // const [sections, setSections] = useState();
-  const notSections = ["Sections 1", "Section 2", "Section 3"]
+  const [sections, getSections] = useState('')
+  // const notSections = ["Sections 1", "Section 2", "Section 3"]
   
 
-	// const allSections = async () => {
-	// 	const res = await fetch('https://bazaar-server.herokuapp.com/api/sections', {
-  //     method: 'GET',
-  //   });
-
-  //   const json = await res.json();
-
-  //   setSections(json.data);
-
-	// };
-
-  
-  // allSections()
-  // console.log(sections)
+  const allSections = async () => {
+    
+		const fetchSections = await axios
+			.get('https://bazaar-server.herokuapp.com/api/sections')
+			.then((res) => {
+        getSections(res.data)
+      })
+    return [sections]  
+    }
   return (
     <div>
-      {notSections.map(section => {
+      {sections.map(section => {
         return(
-          <div className="eachSection">{section}</div>
+          <div className="eachSection">{section.title}</div>
         )}
       )}
     </div>
