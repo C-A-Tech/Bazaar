93a502937f534a2c68619ea5751fd3d1935a3195
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-10 14:55:39 +0100

    Set-up Sections page and included navbar. API fetch request from server side not rendering on page atm, so using a dummy array in the meantime.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Navbar from './components/navbar/index';
 import LandingPage from './components/pages/LandingPage';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
+import Sections from './components/pages/sections/Sections';
 import PageNotFound from './components/pages/PageNotFound';
 
 
@@ -11,15 +13,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <Switch>
-        <Route path="/signup" exact component={SignUp}/>  
-        <Route path="/signin" exact component={SignIn}/>  
-        <Route path="/"  exact component={LandingPage}/>
-        <Route path="*" component={PageNotFound}/>  
+        <div className="Nav-bar">
+          <Navbar />
+        </div>
+        <div className="Routes">
+          <Switch>
+            <Route path="/sections" exact component={Sections}/>  
+            <Route path="/signup" exact component={SignUp}/>  
+            <Route path="/signin" exact component={SignIn}/>  
+            <Route path="/"  exact component={LandingPage}/>
+            <Route path="*" component={PageNotFound}/>  
 
-        
-      </Switch>
-
+            
+          </Switch>
+        </div>
       </div>
       
     </Router>
