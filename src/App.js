2e3e84b5330124deae5dcf83900a73fae63148f6
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-10 21:38:26 +0100

    Compeleted design for landing page. Added logo and sign in and up buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Navbar from './components/navbar/index';
-import LandingPage from './components/pages/LandingPage';
+import LandingPage from './components/pages/landing-page/LandingPage';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
 import Sections from './components/pages/sections/Sections';
@@ -11,25 +11,23 @@ import PageNotFound from './components/pages/PageNotFound';
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
-        <div className="Nav-bar">
-          <Navbar />
-        </div>
-        <div className="Routes">
+        <Router>
           <Switch>
-            <Route path="/sections" exact component={Sections}/>  
             <Route path="/signup" exact component={SignUp}/>  
             <Route path="/signin" exact component={SignIn}/>  
             <Route path="/"  exact component={LandingPage}/>
-            <Route path="*" component={PageNotFound}/>  
-
+            <div className="routes-with-navbar">
+              <Navbar />
+              <Route path="/sections" exact component={Sections}/> 
+            </div>  
             
           </Switch>
-        </div>
+        </Router>
       </div>
       
-    </Router>
+    
   );
 }
 
---
+++ src/components/pages/landing-page/LandingPage.js
@@ -0,0 +1,24 @@
+import {Link } from "react-router-dom";
+import './LandingPage.css'
+import logo from './src/logo.png'
+
+function LandingPage() {
+  
+  return (
+    <div className="LandingPage">
+      <div className="logo">
+        <img src={logo}></img>
+      </div>
+      <br />
+      <div className="signup-in">
+        <Link to="/signin" className="signin"><button>Sign In</button></Link>
+  
+        <Link to="/signup" className="signup"><button>Sign Up</button></Link>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default LandingPage
