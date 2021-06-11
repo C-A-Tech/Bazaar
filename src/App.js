172cfadcfec1f216aa1db50459226f3f73a9c702
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-06-11 02:15:19 +0100

    Completed sign in page design

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Navbar from './components/navbar/index';
 import LandingPage from './components/pages/landing-page/LandingPage';
-import SignIn from './components/pages/SignIn';
-import SignUp from './components/pages/SignUp';
+import SignIn from './components/pages/sign-in/SignIn';
+import SignUp from './components/pages/sign-up/SignUp';
 import Sections from './components/pages/sections/Sections';
 import PageNotFound from './components/pages/PageNotFound';
 
