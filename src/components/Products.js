bbcb63f988731fc583f3a175a2477ae819714a6c
fizzy-fifs <fkinyemi@gmail.com>
2021-06-15 22:35:17 +0100

    Products open as a modal when clicked

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'; 
 import axios from 'axios';
+import Modal from 'react-modal';
+import { FaTimes } from 'react-icons/fa';
 
 const useProducts = () => {
 	const [products, setProducts] = useState([]);
@@ -17,16 +19,41 @@ const useProducts = () => {
 
 function Products(props) {
   const [products] = useProducts();
+  const [modalState, setmodalState] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState('')
+  
+  const selectedProduct = (id) => {
+    setmodalState(true)
+    setSelectedProductId(id)
+  
+    return selectedProductId;
+  }
+  
+  
   return (
     <div>
+      {/* .............................Displays All Products....................................... */}
       {products.filter((product) => product.stall === props.stall).map(filteredProduct => 
         (
-        <div className="eachProduct"> 
-          <img src={filteredProduct.image}/>
-          <h1 className="eachProductName"> {filteredProduct.name} </h1> 
-          <p className="eachProductDescription"> {filteredProduct.description} </p>
-          <p className="eachProductPrice"> {filteredProduct.price} </p>
-        </div>
+          <>
+            <div className="eachProduct" onClick={ () => selectedProduct(filteredProduct._id) }> 
+              <img src={filteredProduct.image}/>
+              <h1 className="eachProductName"> {filteredProduct.name} </h1> 
+              <p className="eachProductDescription"> {filteredProduct.description} </p>
+              <p className="eachProductPrice"> Price: £{filteredProduct.price} </p>
+            </div>
+            { products.filter((product) => product._id === selectedProductId).map((filteredProduct) => (
+              
+              <Modal id="productModal" isOpen={modalState} onRequestClose={() => setmodalState(false)} style={{ overlay: {backgroundColor: 'grey'} }}>
+                <FaTimes style={{color: 'red', cursor: 'pointer', display: 'inline'}} onClick={() => setmodalState(false)}  />
+                <img src={filteredProduct.image}/>
+                <h1 className="eachProductName"> {filteredProduct.name} </h1> 
+                <p className="eachProductDescription"> {filteredProduct.description} </p>
+                <p className="eachProductPrice"> Price: £{filteredProduct.price} </p>
+              
+              </Modal>
+            ) ) }
+          </>
         ))
       }
     </div>
