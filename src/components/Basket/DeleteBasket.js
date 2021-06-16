7ca5a420ec317ecd60fea13a5ca749473fcb327b
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-06-17 00:54:32 +0100

    basket checkout feature now complete

--- src/components/Basket/DeleteBasket.js
+++ src/components/Basket/DeleteBasket.js
@@ -4,28 +4,14 @@ import addNotification from '../../notices/notice';
 import { Redirect } from 'react-router';
 import { useState } from 'react';
 
-const cookies = new Cookies();
-const signedInUser = cookies.get('user');
-const userId = signedInUser._id;
-
 function DeleteBasket(props) {
-	console.log(props.basketId)
+	console.log(props.basketId);
 	const [redirect, setRedirect] = useState(false);
 
 	const deleteCurrentBasket = async () => {
-		const userJson = JSON.stringify({
-			user: userId
-		});
-		const config = {
-			headers: { 'content-type': 'application/json' }
-		};
-
 		await axios
 			.delete(
-				// `https://bazaar-server.herokuapp.com/api/basket/delete/{props.basketId}`,
-				`http://localhost:5000/api/basket/delete/${props.basketId}`,
-				// userJson,
-				// config
+				`https://bazaar-server.herokuapp.com/api/basket/delete/${props.basketId}`
 			)
 			.then((res) => res.data)
 			.then((data) => {
