0b2874c9cbad7a851e8886ddd0ed4171eab022f6
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-06-16 20:43:28 +0100

    basket add working, for 2nd item to work we have to refresh the page

--- src/components/Basket/AddToBasket.js
+++ src/components/Basket/AddToBasket.js
@@ -1,52 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
-import Modal from 'react-modal';
-import { FaTimes } from 'react-icons/fa';
 
-const cookies = new Cookies
+const cookies = new Cookies();
+let signedInUser = cookies.get('user');
 
-let signedInUser = cookies.get('user')
+const submit = async (basket) => {
+	let basketToSend = {
+		name: basket.props.product.name,
+		productId: basket.props.product._id,
+		user: signedInUser._id,
+		price: basket.props.product.price,
+		quantity: 1
+	};
 
+	const basketJson = JSON.stringify(basketToSend);
+  console.log(basketJson);
+	await axios.post(
+		'https://bazaar-server.herokuapp.com/api/basket/add',
+		basketJson,
+		{
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+	);
+};
 
 function AddToBasket(props) {
-  const [basket, setBasket] = useState([])
-  
-  let user = signedInUser._id
+	const [basket, setBasket] = useState([]);
 
- const submit = () => {    
-    
-    setBasket({ ...basket, price: props.product.price })
-    setBasket({ ...basket, productId: props.product._id })
-    setBasket({ ...basket, user: user })
-    setBasket({ ...basket, quantity: 1 })
-    setBasket({ ...basket, name: props.product.name })
-    
-    console.log(basket)
-  
-  
-    const submitBasket = async (props) => {
-      
-      const config = {     
-        headers: { 'content-type': 'application/json' }
-      }
-
-      const jsonBasket = JSON.stringify(basket)
-
-      console.log(jsonBasket)
-    
-      await axios
-        .post('https://bazaar-server.herokuapp.com/api/basket/add', jsonBasket, config)
-        .then((res) => res.data);
-    };
+	return (
+		<div>
+			<button
+				className='addToBasket'
+				onClick={() => {
+					setBasket({ ...basket, props });
+					submit(basket);
+				}}
+			>
+				{' '}
+				Add to Basket{' '}
+			</button>
+		</div>
+	);
 }
 
-  return (
-    <div>
-      <button className="addToBasket" onClick={ (() => submit(props)) } > Add to Basket </button>
-
-    </div>
-  )
-}
-
-export default AddToBasket
+export default AddToBasket;
